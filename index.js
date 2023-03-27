899822c5e9ecab1f67dd511f90424a31d4d6274f
maria hammes <maria.cho@aluno.ifsc.edu.br>
2023-03-27 11:50:25 +0000

    Programar as telas de abertura e d encerramento#20

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// Configuração do jogo
 import config from "./config.js";
+// Cena de abertura
+import CenaDeAbertura from ",/cena.js";
+// Cena principal
+import CenaPrincipal from ",/cena.js";
+// Cena de Encerramento
+import CenaDeencerramento from ",/cena.js";
 
 class Game extends Phaser.Game{
   constructor() {
     super(config);
+    //
+    // Carregar as cenas
+    this.scene.add("abertura", CenaDeAbertura);
+    this.scene.add("Principal", CenaPrincipal);
+    this.scene.add("Encerramento", CenaDeencerramento);
+    //
+    // Iniciar pela cena de abertura
+    this.scene.start("abertura");
   }
 }
 
