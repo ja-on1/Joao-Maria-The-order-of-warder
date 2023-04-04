d201906f7aabfc34027053c9fb29eb30272f881f
João Victor Lauth <joao.l2003@aluno.ifsc.edu.br>
2023-04-04 14:19:23 +0000

    Teste de Mapa

--- cliente/js/cena-principal.js
+++ cliente/js/cena-principal.js
@@ -4,6 +4,17 @@ export default class principal extends Phaser.Scene {
   }
 
   preload() {
+    //
+    // Mapa
+    // Tilemap
+    this.load.tilemapTiledJSON("mapa-principal", "./assets/chao.json");
+    //
+    // Tilesets
+    this.load.image("chao", "./assets/tileset.png");
+    this.load.image("casa", "./assets/casa.png");
+    this.load.image("mochila", "./assets/Mochila.png");
+    //
+    //
     this.load.spritesheet("João", "./assets/players/João.png", {
       frameWidth: 28,
       frameHeight: 55,
@@ -16,6 +27,36 @@ export default class principal extends Phaser.Scene {
   }
 
   create() {
+    //Mapa
+    //Tilemap
+    this.mapa_principal = this.make.tilemap({
+      key: "mapa-principal",
+    });
+    //tilesets
+    this.tileset_principal_chao =
+      this.mapa_principal.addTilesetImage("chao", "chao");
+    this.tileset_principal_casa =
+      this.mapa_principal.addTilesetImage("casa", "parede");
+    this.tileset_principal_mochila =
+      this.mapa_principal.addTilesetImage("mochila", "parede");
+
+  //Layer 0: chão
+    this.chao = this.mapa_principal.createLayer(
+      "chao",
+      this.tileset_principal_chao,
+      0,
+      0
+    );
+    //Layer 1: parede
+    this.parede = this.mapa_principal.createLayer(
+      "parede",
+      this.tileset_principal_parede,
+      0,
+      0
+    );
+
+      //
+      // jogador 1
     this.jogador_1 = this.physics.add.sprite(200, 225, "João");
     this.anims.create({
       key: "jogador-1-baixo",
