8cd6f12ef73c39d78d0686a9a0370ece48a6a5bd
maria hammes <maria.cho@aluno.ifsc.edu.br>
2023-04-10 10:59:39 +0000

    correção prefixo

--- cliente/js/cena-principal.js
+++ cliente/js/cena-principal.js
@@ -104,49 +104,7 @@ export default class principal extends Phaser.Scene {
     //
     // Jogador 2 
     this.jogador_2 = this.add.sprite(400, 225, "Maria");
-    this.anims.create({
-      key: "jogador-2-baixo",
-      frames: this.anims.generateFrameNumbers("Maria", {
-        start: 0,
-        end: 3,
-      }),
-      frameRate: 7,
-      repeat: -1,
-    });
-    this.jogador_2.anims.play("jogador-2-baixo", true);
-    //
-    this.anims.create({
-      key: "jogador-2-cima",
-      frames: this.anims.generateFrameNumbers("Maria", {
-        start: 4,
-        end: 7,
-      }),
-      frameRate: 7,
-      repeat: -1,
-    });
-    this.jogador_2.anims.play("jogador-2-cima", true);
-    //
-    this.anims.create({
-      key: "jogador-2-esquerda",
-      frames: this.anims.generateFrameNumbers("Maria", {
-        start: 8,
-        end: 11,
-      }),
-      frameRate: 7,
-      repeat: -1,
-    });
-    this.jogador_2.anims.play("jogador-2-esquerda", true);
-    //
-    this.anims.create({
-      key: "jogador-2-direita",
-      frames: this.anims.generateFrameNumbers("Maria", {
-        start: 12,
-        end: 15,
-      }),
-      frameRate: 7,
-      repeat: -1,
-    });
-    this.jogador_2.anims.play("jogador-2-direita", true);
+    
   }
 
   update() {}
